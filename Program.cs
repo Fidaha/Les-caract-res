753e344c371b2a0e5043fe6936852107ed4c1247
Fidaha <81554525+Fidaha@users.noreply.github.com>
2023-06-02 10:40:27 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿namespace LesCaracteres;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+           for (int i = 97; i <= 122; i++)
+        {
+            char letter = (char)i;
+            Console.WriteLine(letter + " = '" + letter + "'");
+        }
+    }
+}
